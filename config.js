60eee89a0f4d5aae13d11e7eb00bf194313edd3c
Orji <ndubuisiaso@gmail.com>
2023-06-22 20:30:28 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,14 @@
+const firebase = require("firebase");
+const firebaseConfig = {
+  apiKey: "AIzaSyBxPPdfvrY_SrHBMml_hojWmFa-nlyFvSE",
+  authDomain: "viewbond-48dd9.firebaseapp.com",
+  projectId: "viewbond-48dd9",
+  storageBucket: "viewbond-48dd9.appspot.com",
+  messagingSenderId: "407373777340",
+  appId: "1:407373777340:web:ce485d153bf5d3d90583b9",
+  measurementId: "G-0G9VV0VCSF"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const User = db.collection("Users");
+module.exports = User;
